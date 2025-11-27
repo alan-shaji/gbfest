e574e292d0c323aade9d9c42784166af892a21f5
Allen Shaji <allen0598@gmail.com>
2025-11-26 22:27:53 -0500

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/gbfest/",  // ✅ matches your repo name
   plugins: [react()],
-})
+});
+
+
+
+
